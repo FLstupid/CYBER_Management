8486d4d03216676d2fe0d6d96764c6aa853b94bd
Le Tran Minh Nhut <61163065+FLstupid@users.noreply.github.com>
2022-07-02 12:50:22 +0700

    New ver

--- Project/Tiệm nét/Program.cs
+++ Project/Tiệm nét/Program.cs
@@ -16,7 +16,7 @@ namespace Tiệm_nét
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Login());
+            Application.Run(new Statistic());
         }
     }
 }
